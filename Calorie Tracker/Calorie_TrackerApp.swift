d3a513829b6cd10f7e0a2c4c978188cf1520875a
micahbb05 <micahbb05@gmail.com>
2026-02-27 11:20:34 -0600

    Initial Commit

---
+++ Calorie Tracker/Calorie_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calorie_TrackerApp.swift
+//  Calorie Tracker
+//
+//  Created by Micah Bockisch on 2/27/26.
+//
+
+import SwiftUI
+
+@main
+struct Calorie_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
